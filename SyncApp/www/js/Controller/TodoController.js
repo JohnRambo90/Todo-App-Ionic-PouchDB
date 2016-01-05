8be353b67072cf7ade69ead7d99543971a962cc7
John Marco Dola <j.m.dola1990@gmail.com>
2016-01-05 20:13:19 +0100

    added Delete Function

--- SyncApp/www/js/Controller/TodoController.js
+++ SyncApp/www/js/Controller/TodoController.js
@@ -4,23 +4,35 @@
 angular.module("TodoCtrl", [])
 
 
-  .controller("TodoCtrl", function($scope, $ionicPopup) {
+  .controller("TodoCtrl", function ($scope, $ionicPopup) {
     $scope.tasks = [
-      { title: 'Collect coins' },
-      { title: 'Eat mushrooms' },
-      { title: 'Get high enough to grab the flag' },
-      { title: 'Find the Princess' }
+      {title: 'Collect coins'},
+      {title: 'Eat mushrooms'},
+      {title: 'Get high enough to grab the flag'},
+      {title: 'Find the Princess'}
     ];
-    $scope.addTask = function (){
+    $scope.addTask = function () {
 
       $ionicPopup.prompt({
         title: 'Enter a Task',
         inputPlaceholder: 'Enter a Task!'
-      }).then(function(res) {
-        $scope.tasks.push({title : res});
+      }).then(function (res) {
+        if (!res) {
+          ($ionicPopup.alert({
+            title: "Please Enter a Task!"
+          }))
+        }
+        else {
+          $scope.tasks.push({title: res});
+        }
       });
 
 
     }
 
-})
+
+    $scope.shouldShowDelete = false;
+  $scope.showDelete = function (){
+    $scope.shouldShowDelete = !$scope.shouldShowDelete;
+  }
+  })
